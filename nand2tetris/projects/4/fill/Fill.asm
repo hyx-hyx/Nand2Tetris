5fd41b3e9c93ba275a2a990085a411ce3621d6e7
hyx-hyx <315024424@qq.com>
2025-04-05 19:58:37 +0800

    Fill.asm完成

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -8,8 +8,55 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
+//// Replace this comment with your code.
+(LOOP)
+@0          
+D=A;
+@v
+M=D;        //set v value that will be filled in screen
 
-@24576
-D=M
+@KBD
+D=M;        
+
+
+@BLACK      //if keyborad>0 then v=-1;
+D;JGT
+
+(FILL)      
+@SCREEN
+D=A;
+@address
+M=D;        //set pointer address = screen
+
+@8192
+D=A;
+@c
+M=D;        // set count=8192(256*512/16)
+
+(FILLLOOP)
+@v 
+D=M;
+@address
+A=M;        
+M=D;          //RAM[address]=v;
+
+@1
+D=A;
+@address
+M=M+D;        //address=address+32
+
+@c
+MD=M-1;           
+@FILLLOOP
+D;JGT         // if(c>0) jump to whiteloop
+
+@LOOP
+0;JMP
+
+(BLACK)
 @0
-D;JE
+D=A;
+@v
+M=D-1;
+@FILL
+0;JMP         //set v=-1;
