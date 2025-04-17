e013fb8d84343ab22548a13e59235131528a05cc
hyx-hyx <315024424@qq.com>
2025-04-17 18:34:12 +0800

    添加目录遍历函数

--- nand2tetris/projects/VMTranslator.cpp
+++ nand2tetris/projects/VMTranslator.cpp
@@ -1,10 +1,35 @@
 #include<iostream>
 #include<fstream>
+#include<filesystem>
 #include"parser.h"
 #include"code_write.h"
 using namespace std;
+
+vector<string> listFiles(const filesystem::path& directory) {
+    vector<string> filepath;
+    // 检查目录是否存在
+    if (!filesystem::exists(directory) || !filesystem::is_directory(directory)) {
+        std::cerr << "指定路径不存在或不是一个目录：" << directory << std::endl;
+        return filepath;
+    }
+
+    // 遍历目录
+    for (const auto& entry : filesystem::directory_iterator(directory)) {
+        if (filesystem::is_regular_file(entry)
+            &&entry.path().filename().extension().string()==".vm") { // 判断是否是普通文件
+            filepath.push_back(entry.path().string());
+        } else if (filesystem::is_directory(entry)) { // 判断是否是子目录
+            vector<string> subfilepath=listFiles(entry);
+            filepath.reserve(filepath.size() + subfilepath.size());
+            filepath.insert(filepath.end(), subfilepath.begin(), subfilepath.end());
+        }
+    }
+    return filepath;
+}
+
 int main(){
-    string path="StackArithmetic\\StackTest\\StackTest.vm";
+    vector<string> filepath=listFiles(".\\8\\FunctionCalls\\StaticsTest");
+    string path=filepath[0];
     ifstream in(path,ios::in);
     parser p;
     string filename=path.substr(path.find_last_of('\\')+1,path.find_last_of('.')-path.find_last_of('\\')-1);
