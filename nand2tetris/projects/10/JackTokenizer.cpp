d92536b8bf9cedb5de2e5696a4f462ca65f70ab0
hyx-hyx <315024424@qq.com>
2025-04-23 22:54:58 +0800

    project10 处理注释

--- nand2tetris/projects/10/JackTokenizer.cpp
+++ nand2tetris/projects/10/JackTokenizer.cpp
@@ -1,4 +1,5 @@
 #include "JackTokenizer.h"
+#include<regex>
 
 Token::Token(LEXICAL_ELEMENTS type,string val) : type(type),val(val){}
 Token::Token(){}
@@ -17,48 +18,55 @@ JackTokenizer::JackTokenizer(string filename){
 
 bool JackTokenizer::has_more_token()
 {
+    //regex line_comment_pattern(R"(/\**[\s\S]*?\*/)");
     Token token;
     char c;
     string line;
-    getline(in,line);
-    
-    while(in.get(c)){
-        if(c=='/'){
-            
+    while(getline(in,line)){
+        line.erase(remove_if(line.begin(),line.end(),::isspace),line.end());
+        if(line.empty()){continue;}
+        if(line[0]=='/'&&line[1]=='/'){
+            continue;
         }
-        if(c==' '){continue;}
-        
-        if(c=='"'){
-            while(in.get(c)&&c!='"'){ token.val+=c;}
-            token.type=STRINGCONSTANT;
-            return true;
+        if(line[0]=='/'&&line[1]=='*'){
+            int pos=line.find("*/");
+            line=line.substr(pos+2,line.size()-pos);
         }
-
-        if(isdigit(c)){
-            token.val+=c;
-            while(in.get(c)&&isdigit(c)){ token.val+=c;}
-            token.type=INTEGERCONSTANT;
-            return true;
-        }
-
-        if(symbol.count(token.val)){
-            cur_token=Token(SYMBOL,token.val);
-            return true;
-        }   
-        if(isalpha(c)||c=='_'){
-            token.val+=c;
-            while(in.get(c)&&(isdigit(c)||isalpha(c)||c=='_')){ 
-                token.val+=c;
-                if(keyword.count(token.val)){
-                    cur_token=Token(KEYWORD,token.val);
-                    token.val="";
-                    return true;
-                }
+        string token_val;
+        int pos=0;
+        while(pos<line.size()){
+            if(c=='"'){
+                while(in.get(c)&&c!='"'){ token.val+=c;}
+                token.type=STRINGCONSTANT;
+                return true;
             }
-            cur_token=Token(IDENTIFIER,token.val);
-            return true;
+
+            if(isdigit(c)){
+                token.val+=c;
+                while(in.get(c)&&isdigit(c)){ token.val+=c;}
+                token.type=INTEGERCONSTANT;
+                return true;
+            }
+
+            if(symbol.count(token.val)){
+                cur_token=Token(SYMBOL,token.val);
+                return true;
+            }   
+            if(isalpha(c)||c=='_'){
+                token.val+=c;
+                while(in.get(c)&&(isdigit(c)||isalpha(c)||c=='_')){ 
+                    token.val+=c;
+                    if(keyword.count(token.val)){
+                        cur_token=Token(KEYWORD,token.val);
+                        token.val="";
+                        return true;
+                    }
+                }
+                cur_token=Token(IDENTIFIER,token.val);
+                return true;
+            }
         }
-    }
+    }   
     return false;
 }
 
